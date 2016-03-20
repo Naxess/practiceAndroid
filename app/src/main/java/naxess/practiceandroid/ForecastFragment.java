546e127680ed8419f08ebdd5e1249eb5880ffa0a
Naxess <naxess@gmail.com>
2016-03-20 13:35:22 -0400

    02/17/2016 12:23AM

--- app/src/main/java/naxess/practiceandroid/ForecastFragment.java
+++ app/src/main/java/naxess/practiceandroid/ForecastFragment.java
@@ -13,7 +13,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,6 +35,9 @@ import java.util.List;
 
 public class ForecastFragment extends Fragment
 {
+    Button enter;
+    EditText zipCode;
+
     static ArrayAdapter<String> a;
     public ForecastFragment()
     {
@@ -51,11 +57,14 @@ public class ForecastFragment extends Fragment
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        TextView zipCode = (TextView) getView().findViewById(R.id.zip_code);
+        String zipCodeString = zipCode.getText().toString();
+        //int zipCodeParsed = Integer.parseInt(zipCodeString);
         int id = item.getItemId();
         if(id == R.id.action_refresh)
         {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("11788"); //Need to find a way to grab the zip code from MainActivity.java
+            weatherTask.execute(zipCodeString); //Need to find a way to grab the zip code from MainActivity.java. String resources unmodifiable.
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -64,7 +73,6 @@ public class ForecastFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        //'\u2109' for Fahrenheit symbol and '\u2103' for Celsius symbol
         String[] filler = {
                 "Enter zip code to see forecast"
         };
@@ -77,26 +85,36 @@ public class ForecastFragment extends Fragment
         ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
         lv.setAdapter(a);
 
+        enter = (Button) rootView.findViewById(R.id.enter_button);
+        zipCode = (EditText) rootView.findViewById(R.id.zip_code);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String zipCodeString = zipCode.getText().toString();
+                    FetchWeatherTask weatherTask = new FetchWeatherTask();
+                    weatherTask.execute(zipCodeString);
+                } catch (NumberFormatException e) {
+                    zipCode.setText("");
+                    zipCode.setHint("Invalid zip code.");
+                }
+            }
+        });
+
         return rootView;
     }
     public class FetchWeatherTask extends AsyncTask<String,Void,String[]>
     {
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-        /* The date/time conversion code is going to be moved outside the asynctask later,
-         * so for convenience we're breaking it out into its own method now.
-         */
-        private String getReadableDateString(long time){
-            // Because the API returns a unix timestamp (measured in seconds),
-            // it must be converted to milliseconds in order to be converted to valid date.
+
+        private String getReadableDateString(long time)
+        {
             SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE, MMM dd");
             return shortenedDateFormat.format(time);
         }
 
-        /**
-         * Prepare the weather high/lows for presentation.
-         */
         private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
 
@@ -104,16 +122,8 @@ public class ForecastFragment extends Fragment
             return highLowStr;
         }
 
-        /**
-         * Take the String representing the complete forecast in JSON Format and
-         * pull out the data we need to construct the Strings needed for the wireframes.
-         *
-         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
-         * into an Object hierarchy for us.
-         */
         private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)throws JSONException
         {
-            // These are the names of the JSON objects that need to be extracted.
             final String OWM_LIST = "list";
             final String OWM_WEATHER = "weather";
             final String OWM_TEMPERATURE = "temp";
@@ -124,53 +134,34 @@ public class ForecastFragment extends Fragment
             JSONObject forecastJson = new JSONObject(forecastJsonStr);
             JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
 
-            // OWM returns daily forecasts based upon the local time of the city that is being
-            // asked for, which means that we need to know the GMT offset to translate this data
-            // properly.
-
-            // Since this data is also sent in-order and the first day is always the
-            // current day, we're going to take advantage of that to get a nice
-            // normalized UTC date for all of our weather.
-
             Time dayTime = new Time();
             dayTime.setToNow();
 
-            // we start at the day returned by local time. Otherwise this is a mess.
             int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
 
-            // now we work exclusively in UTC
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
             for(int i = 0; i < weatherArray.length(); i++) {
-                // For now, using the format "Day, description, hi/low"
                 String day;
                 String description;
                 String highAndLow;
 
-                // Get the JSON object representing the day
                 JSONObject dayForecast = weatherArray.getJSONObject(i);
 
-                // The date/time is returned as a long.  We need to convert that
-                // into something human-readable, since most people won't read "1400356800" as
-                // "this saturday".
                 long dateTime;
-                // Cheating to convert this to UTC time, which is what we want anyhow
                 dateTime = dayTime.setJulianDay(julianStartDay+i);
                 day = getReadableDateString(dateTime);
 
-                // description is in a child array called "weather", which is 1 element long.
                 JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
                 description = weatherObject.getString(OWM_DESCRIPTION);
 
-                // Temperatures are in a child object called "temp".  Try not to name variables
-                // "temp" when working with temperature.  It confuses everybody.
                 JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
                 highAndLow = formatHighLows(high, low);
-                resultStrs[i] = day + " - " + description + " - " + highAndLow + "\u2103";
+                resultStrs[i] = day + " - " + description + " - (" + highAndLow + "\u2109" + ")";
             }
             for (String s : resultStrs)
             {
@@ -187,30 +178,17 @@ public class ForecastFragment extends Fragment
                 return null;
             }
 
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
-            // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
 
             String format = "json";
-            String units = "metric";
+            String units = "imperial";
             int numDays = 14;
 
             try
             {
-                    // Construct the URL for the OpenWeatherMap query
-                    // Possible parameters are avaiable at OWM's forecast API page, at
-                    // http://openweathermap.org/API#forecast
-                //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=11788&mode=json&units=metric&cnt=7id=524901&APPID=fbdb1bd9a21f79aa5ab46b5739ee96fc");
-                /* Alternative URL format to the above line
-                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=11788&mode=json&units=metric&cnt=7";
-                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
-                URL url = new URL(baseUrl.concat(apiKey));
-                */
-                //Another Alternative URL format that replaces both above. This one allows for changes to individual parameters
                 final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
                 final String QUERY_PARAM = "zip"; //changed from 'q'. 'q' gives a different city: "Askola"
                 final String FORMAT_PARAM = "mode";
@@ -229,17 +207,14 @@ public class ForecastFragment extends Fragment
 
                 Log.v(LOG_TAG,"Built URI " + builtUri.toString());
 
-                // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
-                // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
                 if (inputStream == null)
                 {
-                    // Nothing to do.
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -247,15 +222,11 @@ public class ForecastFragment extends Fragment
                 String line;
                 while ((line = reader.readLine()) != null)
                 {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
                     buffer.append(line + "\n");
                 }
 
                 if (buffer.length() == 0)
                 {
-                    // Stream was empty.  No point in parsing.
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
@@ -264,8 +235,6 @@ public class ForecastFragment extends Fragment
             catch (IOException e)
             {
                 Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
                 return null;
             }
             finally
--- app/src/main/java/naxess/practiceandroid/MainActivity.java
+++ app/src/main/java/naxess/practiceandroid/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -27,6 +25,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {Snackbar.make(view, "", Snackbar.LENGTH_LONG).setAction("", null).show();}
         });
 
+        /*
         Button enter = (Button) findViewById(R.id.enter_button);
         enter.setOnClickListener(new View.OnClickListener()
         {
@@ -37,8 +36,8 @@ public class MainActivity extends AppCompatActivity
                 {
                     EditText zipCode = (EditText) findViewById(R.id.zip_code);
                     String zipCodeString = zipCode.getText().toString();
-                    int zipCodeParsed = Integer.parseInt(zipCodeString);
-                    zipCode.setText("You entered: " + zipCodeParsed);
+                    //ForecastFragment forecastFragment = (ForecastFragment) getFragmentManager().findFragmentByTag("forecastFragment");
+                    //forecastFragment.FetchWeatherTask(zipCodeString);
                 }
                 catch(NumberFormatException e)
                 {
@@ -48,12 +47,12 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+        */
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -61,12 +60,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
         {
             return true;
